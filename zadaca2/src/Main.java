362008caa26f1a1db7a60e784e06c3c910d20e65
Riad <riadcosic@outlook.com>
2023-03-21 22:18:32 +0100

    Second commit, but wait there is more..

--- zadaca2/src/Main.java
+++ zadaca2/src/Main.java
@@ -1,5 +1,62 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       /* int amountInCashRegister = 99;
+        int checkAmount = 66;
+        int KM20 = 20;
+        int KM10 = 10;
+        int KM2=2;
+        int KM1=1;
+        int moneyAmount = 100;
+        int changeAmount;
+
+
+
+        if ((amountInCashRegister > 100) &&  (checkAmount >0)){
+            System.out.println("You have too much money");
+        }       else if (checkAmount<=0) {
+             System.out.println("You didnt buy anything did you");
+        }       else {
+                 changeAmount = moneyAmount - checkAmount;
+                     System.out.println("Your change is " + changeAmount + " KM");
+                        KM20=changeAmount%20;
+                        System.out.println(KM20);
+
+                     }
+        if (checkAmount<0) {
+
+            System.out.println("Did you maybe break something");
+        }
+*/
+
+
+            // Zadatak 2. - površina kruga
+
+         /* double areaOfCircle;
+                double radiusOfCircle=0.1;
+                double piNumber = 3.14;
+                if (radiusOfCircle>0) {
+            areaOfCircle = radiusOfCircle * radiusOfCircle * piNumber;
+            System.out.println("Površina kruga je:  " + areaOfCircle);
+        }   else {
+                    System.out.println("Radijus kruga treba biti veći od nule !");
+                }  */
+
+            // Zadatak 3. - djeljivost brojeva
+
+        /*   int A=40;
+            int B=4;
+            if ((A%B == 0) && (A>10) && (A%3 == 0) && (A<10000)) {
+                System.out.println("Broj " + A + " je djeljiv sa " + B );
+            } else  if (A%B ==0){
+                System.out.println("Broj " + A + " je djeljiv sa "+ B + " ali ne zadovoljava ostale uslove !");
+            }  else {
+                System.out.println("Broj " + A + " nije djeljiv sa " + B );
+
+            } */
+
+        
+
+
+        System.out.println(" ");
     }
 }
