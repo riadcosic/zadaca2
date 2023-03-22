a749fc61ee439d0dde59cbef0fe3c545e97c5298
Riad <riadcosic@outlook.com>
2023-03-22 20:58:13 +0100

    Homework

--- zadaca2/src/Main.java
+++ zadaca2/src/Main.java
@@ -7,61 +7,59 @@ public class Main {
         int twoMarksValue = 2;
         int oneMarkValue = 1;
         int moneyAmount = 100;
-        int changeAmount;
-        int runningAmount;
+        int changeAmount, runningAmount;
         int twentyNumberOfBills, tenNumberOfBills, twoNumberOfBills, oneNumberOfBills;
 
 
-        if ((amountInCashRegister > 100) &&  (checkAmount >0)){
-            System.out.println("You have too much money");
-        }       else if (checkAmount<=0) {
-            System.out.println("You didnt buy anything did you");
-        }       else {
-            changeAmount = moneyAmount - checkAmount;
-            System.out.println("Your change is " + changeAmount + " KM");
-            runningAmount=changeAmount;
-            twentyNumberOfBills = runningAmount/twentyMarksValue;
-            runningAmount -= twentyNumberOfBills * twentyMarksValue;
-            tenNumberOfBills =runningAmount/tenMarksValue;
-            runningAmount -= tenNumberOfBills * tenMarksValue;
-            twoNumberOfBills = runningAmount/twoMarksValue;
-            runningAmount -= twoNumberOfBills * twoMarksValue;
-            oneNumberOfBills = runningAmount/oneMarkValue;
-            System.out.println("20 KM = " +twentyNumberOfBills);
-            System.out.println("10 KM = " +tenNumberOfBills);
-            System.out.println("2 KM = " +twoNumberOfBills);
-            System.out.println("1 KM = " +oneNumberOfBills);
+        if ((amountInCashRegister > 100) && (checkAmount > 0)) {
+             System.out.println("You have too much money");
+        }               else if (checkAmount <= 0) {
+                        System.out.println("You didnt buy anything did you");
+        }                        else {
+                                        changeAmount = moneyAmount - checkAmount;
+                                        System.out.println("Your change is " + changeAmount + " KM");
+                                            runningAmount = changeAmount;
+                                        twentyNumberOfBills = runningAmount / twentyMarksValue;
+                                             runningAmount -= twentyNumberOfBills * twentyMarksValue;
+                                        tenNumberOfBills = runningAmount / tenMarksValue;
+                                            runningAmount -= tenNumberOfBills * tenMarksValue;
+                                        twoNumberOfBills = runningAmount / twoMarksValue;
+                                             runningAmount -= twoNumberOfBills * twoMarksValue;
+                                        oneNumberOfBills = runningAmount / oneMarkValue;
+                                  System.out.println("20 KM = " + twentyNumberOfBills);
+                                  System.out.println("10 KM = " + tenNumberOfBills);
+                                  System.out.println("2 KM = " + twoNumberOfBills);
+                                  System.out.println("1 KM = " + oneNumberOfBills);
 
         }
-        if (checkAmount<0) {
+        if (checkAmount < 0) {
 
             System.out.println("Did you maybe break something");
         }
 
 
-
         // Zadatak 2. - površina kruga
 
         double areaOfCircle;
-        double radiusOfCircle=0.1;
+        double radiusOfCircle = 0.1;
         double piNumber = 3.14;
-        if (radiusOfCircle>0) {
+        if (radiusOfCircle > 0) {
             areaOfCircle = radiusOfCircle * radiusOfCircle * piNumber;
             System.out.println("Površina kruga je:  " + areaOfCircle);
-        }   else {
+        } else {
             System.out.println("Radijus kruga treba biti veći od nule !");
         }
 
         // Zadatak 3. - djeljivost brojeva
 
-        int A=40;
-        int B=4;
-        if ((A%B == 0) && (A>10) && (A%3 == 0) && (A<10000)) {
-            System.out.println("Broj " + A + " je djeljiv sa " + B );
-        } else  if (A%B ==0){
-            System.out.println("Broj " + A + " je djeljiv sa "+ B + " ali ne zadovoljava ostale uslove !");
-        }  else {
-            System.out.println("Broj " + A + " nije djeljiv sa " + B );
+        int A = 40;
+        int B = 4;
+        if ((A % B == 0) && (A > 10) && (A % 3 == 0) && (A < 10000)) {
+            System.out.println("Broj " + A + " je djeljiv sa " + B);
+        } else if (A % B == 0) {
+            System.out.println("Broj " + A + " je djeljiv sa " + B + " ali ne zadovoljava ostale uslove !");
+        } else {
+            System.out.println("Broj " + A + " nije djeljiv sa " + B);
 
         }
 
@@ -82,8 +80,8 @@ public class Main {
 
         // Zadatak 5.
         int X = 1;
-        boolean variable1 = (((((X+X+6)/2)-X)%2) == 0);
-        if  (variable1 == true) {
+        boolean variable1 = (((((X + X + 6) / 2) - X) % 2) == 0);
+        if (variable1 == true) {
             System.out.println("(((( " + X + " + " + X + " + " + " 6 " + ")" + "/2))" + " - " + X + ") je djeljivo sa 2");
         } else {
             System.out.println("(((( " + X + " + " + X + " + " + " 6 " + ")" + "/2))" + " - " + X + ") nije djeljivo sa 2");
